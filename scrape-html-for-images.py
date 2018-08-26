ade35e348cdf6bbea86c5f26bfdd7965ada881ad
Xristopher Anderton <xris@tikidream.com>
2018-08-26 10:12:54 -0400

    archives parsed and file error handling

--- scrape-html-for-images.py
+++ scrape-html-for-images.py
@@ -11,37 +11,64 @@ from urllib.request import (
     urlopen, urlparse, urlunparse, urlretrieve, urljoin)
 import os
 import sys
+import html
 
-def main(url, out_folder="./_images/"):
+def main(url, out_folder="./_images/", archive_path = "./_archive/"):
     """Downloads all the images at 'url' to ./_images/"""
+    isLocal = False
 
     if url.lower().startswith("http"):
         # remote file
-        print("Opening remote file")
-        soup = bs(urlopen(url))
+        soup = bs(urlopen(url), "html5lib")
     else:
         # local file
-        print("Opening local file")
-        soup = bs(open(url), "html.parser")
+        try:
+            local_file = open(url)
+        except FileNotFoundError:
+            # doesn't exist
+            print("Source file does not exist.")
+            sys.exit()
+        else:
+            # exists
+            soup = bs(local_file, "html5lib")
+            isLocal = True
 
+    # make sure html entities are unescaped
+    soup = bs(html.unescape(soup.prettify(formatter="html5")), "html5lib");
+
+    # create out_folder if not exists
     if not os.path.exists(out_folder):
         print("Directory %s doesn't exist. Creating it now." % out_folder)
         os.makedirs(out_folder)
 
-    parsed = list(urlparse(url))
+    _extractImages(soup)
 
-    print("Checking for images")
+    if isLocal:
+        _archiveParsed(url, archive_path)
+
+    print("Complete.")
+
+def _usage():
+    print("usage: python dumpimages.py http://example.com [outpath]")
+
+def _archiveParsed(file, archive_path):
+    print("archiving source file")
+    # create out_folder if not exists
+    if not os.path.exists(archive_path):
+        print("Archive directory does not exist. Creating it now. (%s)" % archive_path)
+        os.makedirs(archive_path)
+    filename = os.path.basename(file)
+    # print("%s%s" % (archive_path, filename))
+    os.rename(url, archive_path + filename)
+
+def _extractImages(soup):
     print("There are %s images" % len(soup.findAll("img")))
     for image in soup.findAll("img"):
-        print("image")
-        # print "Image: %(src)s" % image
+        print("Image: %(src)s" % image)
         image_url = urljoin(url, image['src'])
         filename = image["src"].split("/")[-1]
         outpath = os.path.join(out_folder, filename)
         urlretrieve(image_url, outpath)
-
-def _usage():
-    print("usage: python dumpimages.py http://example.com [outpath]")
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
